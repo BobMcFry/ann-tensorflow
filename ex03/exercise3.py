4fc51c378f5985de0b56dcaf9c2093c5689d6ae8
rdiederichse <rdiederichse@uos.de>
2017-11-14 17:02:41 +0100

    Add plotting for ex3

--- ex03/exercise3.py
+++ ex03/exercise3.py
@@ -3,6 +3,19 @@ from matplotlib import pyplot as plt
 from cifar_helper import CIFAR
 
 cifar = CIFAR()
-test_batch = cifar.get_test_batch(5)
-__import__('ipdb').set_trace()
+test_batch = list(zip(cifar._training_data[:16], cifar._training_labels[:16]))
+categories = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse',
+        'Ship', 'Truck']
+f, axarr = plt.subplots(4, 4)
+for i in range(4):
+    for j in range(4):
+        index = 4*i+j
+        ax = axarr[i][j]
+        img = test_batch[index][0]
+        label = test_batch[index][1]
+        ax.set_title(categories[label])
+        ax.imshow(img)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+plt.show()
 
